41018b35327d7542675f1e1b1bfa937591693c5c
liu <liunian@2077tech.com>
2023-08-02 15:35:49 +0800

    修复字体文件会被提交的问题

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,7 +1,4 @@
-import DefaultTheme from "vitepress/theme-without-fonts";
-import "./puhuiti.css";
-import "./inter.css";
-import "./custom.css";
+import DefaultTheme from "vitepress/theme";
 import "./custom-color.css";
 
 export default DefaultTheme;
