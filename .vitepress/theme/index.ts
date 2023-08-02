5c0a9cfe94c030a67f68b306b7c1cf20da3d8eff
liunnn1994 <liunnn1994@users.noreply.github.com>
2023-08-02 07:15:25 +0000

    Released v1.0.0

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,4 +1,7 @@
-import DefaultTheme from "vitepress/theme";
+import DefaultTheme from "vitepress/theme-without-fonts";
+import "./puhuiti.css";
+import "./inter.css";
+import "./custom.css";
 import "./custom-color.css";
 
 export default DefaultTheme;
