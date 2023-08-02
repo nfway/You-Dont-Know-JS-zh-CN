4194b4384bb2656d3ac53f8a698f6e3a9bb3e1a0
liu <liunian@2077tech.com>
2023-08-02 15:12:51 +0800

    添加自定义主题

--- .vitepress/theme/vitepress-theme/index.ts
+++
@@ -1,6 +0,0 @@
-import DefaultTheme from "vitepress/theme-without-fonts";
-import "./puhuiti.css";
-import "./inter.css";
-import "./custom.css";
-
-export default DefaultTheme;
--- scripts/vitepress-theme/index.ts
+++ scripts/vitepress-theme/index.ts
@@ -2,5 +2,6 @@ import DefaultTheme from "vitepress/theme-without-fonts";
 import "./puhuiti.css";
 import "./inter.css";
 import "./custom.css";
+import "./custom-color.css";
 
 export default DefaultTheme;
